64ba93008c12f7ae746bd73d8a1dbc9ffe8962c2
Joseph Pranav <pranavjoseph69@gmail.com>
2025-08-24 16:36:30 +0100

    fix: set base path for production environment in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.NODE_ENV === 'production' ? '/pranav_dev/' : '/',
   server: {
     port: 3000
   }
