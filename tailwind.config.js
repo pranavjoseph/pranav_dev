88962ec249f11e6ca788004c6e49ee968cc88774
Joseph Pranav <pranavjoseph69@gmail.com>
2025-08-29 19:08:19 +0100

    fix: correct dark mode configuration in Tailwind CSS setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class', // enable dark mode via a class
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
