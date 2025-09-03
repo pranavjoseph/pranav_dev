7032f8a12e4d0f4af781a59fd7716c60f8c75c49
Joseph Pranav <pranavjoseph69@gmail.com>
2025-09-03 21:15:44 +0100

    feat: CookieConsentBanner implimentation

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './components/ThemeProvider.jsx'
+import CookieConsentBanner from './components/CookieConsent.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <CookieConsentBanner />
     </ThemeProvider>
   </StrictMode>,
 )
